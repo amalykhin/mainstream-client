e1923dae631e200e512bbf1d744670f399d01a19
Andrey Malykhin <andrei.malihin@ati.utm.md>
2020-05-26 01:19:59 +0300

    Implemented chat logic

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+import { UserService } from '../services/user.service';
+import { Stream } from '../services/stream.service';
 
 @Component({
   selector: 'app-chat',
@@ -6,8 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  private hubConnection: HubConnection;
+  private username: string;
+  @Input() currentStream: Stream;
+  chatEntries$ = new BehaviorSubject<any[]>([]);
 
-  constructor() { }
+  constructor(private userService: UserService) { 
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl('https://localhost:5001/chat')
+      .build();
+    this.hubConnection.start();
+    this.hubConnection.on('ReceiveMessage', chatEntry => {
+      console.debug(chatEntry);
+      const entries = this.chatEntries$.value;
+      entries.push(chatEntry);
+      this.chatEntries$
+        .next(entries);
+    });
+
+    this.userService.currentUser$.subscribe(user => this.username = user.username);
+  }
 
   ngOnInit(): void {
   }
@@ -25,5 +47,16 @@ export class ChatComponent implements OnInit {
     textarea.style.height = 'auto';
     const newHeight = textarea.scrollHeight.toString();
     textarea.style.height = newHeight + 'px';
+  }
+
+  sendMessage(textarea: HTMLTextAreaElement, sendButton: HTMLButtonElement) {
+    const message = textarea.value;
+    sendButton.disabled = true;
+    this.hubConnection.invoke("sendmessage", this.username, message)
+      .then(() => {
+        sendButton.disabled = false;
+        textarea.value = '';
+      });
+
   }
 }
